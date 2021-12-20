870d3bde449447e8fae760cf5803f381cdfe1c1d
Martin Tesar <tesar.cze@gmail.com>
2021-12-20 14:28:31 +0100

    change info to git version from tags

--- src/GitInfo.macro.js
+++ src/GitInfo.macro.js
@@ -1,51 +1,24 @@
 const { createMacro } = require('babel-plugin-macros');
 const { execSync } = require('child_process');
 
-const parsedGitLog = (() => {
-  let message;
-  let refs;
-  const commit = {};
-  // only the commit message can have multiple lines. Make sure to always add at the end:
-  // The format is specified in https://git-scm.com/docs/git-log#_pretty_formats
-  let gitCommand = 'git log --format=%D%n%h%n%H%n%cI%n%B -n 1 HEAD';
-  if (process.platform === 'win32') {
-    gitCommand = gitCommand.replace(/%/g, '^%'); // need to escape percents in batch
-  }
+const parsedGitVersionFromTags = (() => {
+
+  let gitCommand = 'git describe --tags --long';
 
   const logResult = execSync(gitCommand)
     .toString()
     .trim()
-    .split(/\r?\n/);
-  [refs, commit.shortHash, commit.hash, commit.date, ...message] = logResult;
-  commit.message = message.join('\n');
-  return {refs, commit};
+
+    const regexp = /([0-9]+)\.([0-9]+)-([0-9]+)(.*)/
+    const [_, major, minor, buildNumber] = logResult.match(regexp);
+
+    return {major, minor, buildNumber};
 })();
 
-const parseRefs = (refs) => {
-  let branch = undefined;
-  const tags = [];
-  refs.split(', ').map((item) => {
-    // if HEAD is not detached, the branch is printed out as `HEAD -> branch_name`.
-    // if HEAD is detached, the output becomes `HEAD`.
-    const isBranch = item.match(/^HEAD -> (.*)$/);
-    const isTag = item.match(/^tag: (.*)$/);
-
-    if (isTag && isTag.length > 1) {
-      tags.push(isTag[1]);
-    } else {
-      branch = isBranch ? isBranch[1] : branch;
-    }
-  });
-
-  return [branch, tags];
-};
-
 const gitInfo = (() => {
-  const ret = {};
+  let ret = {};
   try {
-    const logResult = parsedGitLog;
-    [ret.branch, ret.tags] = parseRefs(logResult.refs);
-    ret.commit = logResult.commit;
+    ret = parsedGitVersionFromTags;
   } catch (e) {
     throw Error(`Unable to parse the git information: ${e}`);
   }
